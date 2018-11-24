9384410d75830cd2931d1afae2b440bda5fb722b
Qifeng Wu <wuqifeng57f@gmail.com>
2018-11-24 02:44:15 +0000

    WishList.php edited online with Bitbucket

--- models/WishList.php
+++ models/WishList.php
@@ -1,5 +1,5 @@
 <?php
-
+include 'Wish.php'
 /**
  * Created by PhpStorm.
  * Date: 11/11/2018
@@ -7,16 +7,19 @@
  */
 class WishList
 {
-    private $wishs = [];
+    private $wishes = [];
 
     /**
      * WishList constructor.
      * @param array $wishs
      */
-    public function __construct(array $wishs)
+    public function __construct(array $wishes)
     {
-        $this->wishs = $wishs;
+        $this->wishes = $wishes;
     }
-
+    public function add(Wish $wish)
+    {
+        array_push($this->wishes,wish);    
+    }
 
 }
