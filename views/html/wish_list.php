56d2d0872d6571263762f36bc2d5e1faede88e81
Qifeng Wu <wuqifeng57f@gmail.com>
2018-12-06 19:44:42 +0000

    wish_list.php edited online with Bitbucket

--- views/html/wish_list.php
+++ views/html/wish_list.php
@@ -8,5 +8,30 @@
 include_once "headband.html";
 ?>
 <body>
-    <h1>Welcome on the wish list page</h1>
+   <div class="container">
+  <h2>Wishlist</h2>
+  <form>
+    <div class="form-group">
+      <ul id = "wl"></ul>
+      <label for="wishlist">Add your wish</label>
+      <textarea class="form-control" rows="3" id="wishlist"></textarea>
+      <button onClick="displaywishlist()"type = "button" class = "btn btn-default">Add</button>
+    </div>
+  </form>
+</div>
+
+<script>
+
+var wishlist_ = []
+function displaywishlist() {
+    var x = document.getElementById("wishlist").value;
+    wishlist_.push(x);
+
+    var ul = document.getElementById("wl");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(x));
+    ul.appendChild(li);
+
+}
+</script>
 </body>
